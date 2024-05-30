1a0678d13c826b41df32f115222b409580df3ace
Leysa Pozo <leysapozo41@gmail.com>
2024-05-30 19:06:39 +0200

    creating the catalog

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("hola")
+// CODIGO CREADO PARA EL CAMBIO DE COLOR DE FONDO DEL MENÚ DE NAVEGACIÓN
+window.onscroll = function (e) {  
+    var menu = document.querySelector(".barra-nav");
+    menu.classList.toggle("change-background", window.scrollY>0);
+} 
